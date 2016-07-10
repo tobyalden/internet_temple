cab7cd328fe51949aae6976d96371d135a49cc0d
Toby <toby@tobys-mbp.domain>
2016-07-10 14:55:03 -0700

    Provide new viewers with current embed code

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+currentEmbed = "https://www.youtube.com/embed/DOvy38gU4Ls?showinfo=0&autoplay=1&rel=0&controls=0&modestbranding=0&disablekb=1";
+
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
@@ -14,15 +16,16 @@ app.get('/about', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-  socket.on('chat message', function(msg) {
-    io.emit('chat message', msg);
+  socket.on('request current embed', function() {
+    io.emit('send current embed', currentEmbed);
   });
   socket.on('embed submission', function(embedCode) {
-    io.emit('embed submission', embedCode);
+    currentEmbed = embedCode;
+    io.emit('embed submission', currentEmbed);
   });
 });
 
 var port = process.env.PORT || 3000;
-http.listen(port, function(){
+http.listen(port, function() {
   console.log('listening on *:3000');
 });
