a69a16c75f922cd5df7a9ab1f3497ebc3c98a202
Toby <toby@tobys-mbp.domain>
2016-07-11 19:06:33 -0700

    Add chat

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var Moniker = require('moniker');
 
 currentEmbed = "https://www.youtube.com/embed/DOvy38gU4Ls?showinfo=0&autoplay=1&rel=0&controls=0&modestbranding=0&disablekb=1";
 
@@ -17,11 +18,15 @@ app.get('/about', function(req, res) {
 
 io.on('connection', function(socket) {
   socket.on('request current embed', function() {
-    socket.emit('send current embed', currentEmbed);
+    socket.emit('send current embed', {'currentEmbed': currentEmbed, 'newUsername': Moniker.choose()});
   });
   socket.on('embed submission', function(embedCode) {
     currentEmbed = embedCode;
     io.emit('embed submission', currentEmbed);
+  });
+  socket.on('send chat message', function(message) {
+    console.log('message recieved: ' + message);
+    io.emit('get chat message', message);
   });
 });
 
